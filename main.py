03e913c361daf7261fc53a18df1f61f24f0fa016
mathieu <mathieu@2027a.net>
2024-10-13 04:18:09 -0400

    update requirements for using groq

--- main.py
+++ main.py
@@ -10,16 +10,17 @@ from llama_index.core import (
     load_index_from_storage,
 )
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
-from llama_index.llms.ollama import Ollama
+from llama_index.llms.groq import Groq
+
+# from llama_index.llms.ollama import Ollama
 
 DATA_DIR = "./data"
 INDEX_DIR = "./storage"
-LLM_MODEL_NAME = "llama3.2:latest"
-
+LLM_MODEL_NAME = "llama-3.1-70b-versatile"
 embedding_name = "mixedbread-ai/mxbai-embed-large-v1"
 embed_model = HuggingFaceEmbedding(model_name=embedding_name)
 
-llm = Ollama(model=LLM_MODEL_NAME, temperature=0.2, request_timeout=220.0)
+llm = Groq(model=LLM_MODEL_NAME, temperature=0.2, request_timeout=220.0)
 Settings.llm = llm
 Settings.embed_model = embed_model
 
@@ -45,13 +46,12 @@ def prepare_template():
     Prepare a prompt template for the QA system.
     """
     text_qa_template_str = """
-    Tu es un scribe médiéval qui utilise un style ancien et d'époque dans sa réponse. Tu es le gardien d'archive très anciennes. Tu réponds aux questions des voyageurs. En voici une : {query_str}
-    Voilà tout ce que tu sais et que tu as archivé à ce sujet :
+    You are a helpfull assistant, specialized in retrieving and synthetizing knowledge. You answer to user questions the best you can. Here is one : {query_str}
+    Here is your knowledge base :
     --------
     {context_str}
     --------
-    À partir de ces connaissances à toi, et uniquement à partir d'elles, réponds en français à la question.
-    Écris une réponse verbeuse.
+    Based on that knowledge base only, you write helpfull, precise and detailled answers
     """
     qa_template = PromptTemplate(text_qa_template_str)
     return qa_template
@@ -62,8 +62,8 @@ st.markdown(
             <img src='https://upload.wikimedia.org/wikipedia/en/a/ae/The_library_of_babel_-_bookcover.jpg' style='display: block; margin-left: auto; margin-right: auto; width: 160px;'>
 
             <div style='text-align: center;'>
-            <h1>Parler avec mon texte</h1>
-            <h5>Discussion avec les archives</h5>
+            <h1>Titre principal</h1>
+            <h5>Titre de niveau 5</h5>
             </div>
             """,
     unsafe_allow_html=True,
@@ -87,9 +87,7 @@ query_engine = index.as_query_engine(text_qa_template=qa_template, similarity_to
 
 if st.session_state.messages[-1]["role"] == "user":
     with st.chat_message("assistant"):
-        with st.spinner(
-            "Excellente question. Je vais aller voir ce que les archives disent."
-        ):
+        with st.spinner("wait spinner..."):
             response = query_engine.query(prompt)
         st.markdown(response.response, unsafe_allow_html=True)
         st.session_state.messages.append(
