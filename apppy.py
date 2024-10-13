f318b83c9b7253b01a5c2d1b34009ad134c0c891
mathieu <mathieu@2027a.net>
2024-10-13 01:11:22 -0400

    initial setup

--- apppy.py
+++ apppy.py
@@ -1,9 +1,16 @@
-from llama_index.core import SimpleDirectoryReader, VectorStoreIndex, StorageContext, load_index_from_storage
-from llama_index.core import PromptTemplate, Settings
+import os
+
+import streamlit as st
+from llama_index.core import (
+    PromptTemplate,
+    Settings,
+    SimpleDirectoryReader,
+    StorageContext,
+    VectorStoreIndex,
+    load_index_from_storage,
+)
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.llms.ollama import Ollama
-import streamlit as st
-import os
 
 DATA_DIR = "./data"
 INDEX_DIR = "./storage"
@@ -12,7 +19,7 @@ LLM_MODEL_NAME = "llama3.2:latest"
 embedding_name = "mixedbread-ai/mxbai-embed-large-v1"
 embed_model = HuggingFaceEmbedding(model_name=embedding_name)
 
-llm = Ollama(model=LLM_MODEL_NAME, temperature = 0.2 ,request_timeout=220.0)
+llm = Ollama(model=LLM_MODEL_NAME, temperature=0.2, request_timeout=220.0)
 Settings.llm = llm
 Settings.embed_model = embed_model
 
@@ -28,6 +35,7 @@ def load_index():
         storage_context = StorageContext.from_defaults(persist_dir=INDEX_DIR)
         index = load_index_from_storage(storage_context)
     return index
+
 
 index = load_index()
 
@@ -49,16 +57,17 @@ def prepare_template():
     return qa_template
 
 
-
-st.markdown("""
+st.markdown(
+    """
             <img src='https://upload.wikimedia.org/wikipedia/en/a/ae/The_library_of_babel_-_bookcover.jpg' style='display: block; margin-left: auto; margin-right: auto; width: 160px;'>
-            
+
             <div style='text-align: center;'>
             <h1>Parler avec mon texte</h1>
             <h5>Discussion avec les archives</h5>
             </div>
-            """
-            , unsafe_allow_html=True)
+            """,
+    unsafe_allow_html=True,
+)
 
 # Initialize session state messages if not already present
 if "messages" not in st.session_state:
@@ -78,7 +87,11 @@ query_engine = index.as_query_engine(text_qa_template=qa_template, similarity_to
 
 if st.session_state.messages[-1]["role"] == "user":
     with st.chat_message("assistant"):
-        with st.spinner("Excellente question. Je vais aller voir ce que les archives disent."):
+        with st.spinner(
+            "Excellente question. Je vais aller voir ce que les archives disent."
+        ):
             response = query_engine.query(prompt)
         st.markdown(response.response, unsafe_allow_html=True)
-        st.session_state.messages.append({"role": "assistant", "content": response.response})
+        st.session_state.messages.append(
+            {"role": "assistant", "content": response.response}
+        )
